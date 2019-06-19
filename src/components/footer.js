7fd968d3c5f2a69d34d2d77c7e75a5d1980e6c45
roni <roni.davelman@gmail.com>
2019-06-19 13:39:03 -0400

    mobile style

--- src/components/footer.js
+++ src/components/footer.js
@@ -12,11 +12,11 @@ export default class Footer extends React.Component {
     return (
       <div className="footer-container">
         <span>
-          <a href="/" target="_blank" className="standalone-link">What is cDAI?</a>
+          <a href="/" target="_blank" className="standalone-link">What is cDAI</a>?
         </span>
         <div className="footer-text">
           cDAI.io searches Kyber, Bancor, Uniswap and Radar Relay. This site is powered by the <a href="https://docs.dex.ag" target="_blank">DEX.AG SDK</a>.
-          Please <a href="https://github.com/SettleFinance/cDAI" target="_blank">fork it.</a>
+          Please <a href="https://github.com/SettleFinance/cDAI" target="_blank">fork it</a>.
         </div>
         <div className="footer-links">
           <a href="https://DEX.AG" target="_blank">DEX.AG</a>|
